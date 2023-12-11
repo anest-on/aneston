139424aac1bfb521a38edec18065385916ab72b1
Matheus Adorno <matheusadorno11@gmail.com>
2023-12-11 14:04:16 -0300

    prisma user table

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -11,7 +11,7 @@ const Register = () => {
           medida em que são agendados.
         </p>
 
-        <MultiStep size={6} currentStep={5} />
+        <MultiStep size={4} currentStep={1} />
       </div>
     </main>
   )
