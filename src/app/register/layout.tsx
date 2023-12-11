96e3d7eb836659f8a353be0b0d0c0f95f0f99413
Matheus Adorno <matheusadorno11@gmail.com>
2023-12-11 08:53:08 -0300

    setting up the colors of the project

---
+++ src/app/register/layout.tsx
@@ -0,0 +1,5 @@
+const RegisterLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-screen bg-gray-900 text-gray-200">{children}</div>
+}
+
+export default RegisterLayout
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,22 @@ const config: Config = {
     fontFamily: {
       sans: 'var(--font-roboto)',
     },
+    colors: {
+      'green-500': '#00B37E',
+      'green-600': '#00875F',
+
+      'red-500': '#EF4444',
+
+      'gray-50': '#F9FAFB',
+      'gray-100': '#E1E1E6',
+      'gray-200': '#A9A9B2',
+      'gray-300': '#9CA3AF',
+      'gray-400': '#7C7C8A',
+      'gray-500': '#6B7280',
+      'gray-600': '#323238',
+      'gray-800': '#202024',
+      'gray-900': '#121214',
+    },
   },
   plugins: [],
 }
