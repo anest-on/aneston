2536e9642cea757fc98f8f37df772382edcf48fd
Matheus Adorno <matheusadorno11@gmail.com>
2023-12-13 23:59:36 -0300

    next oAuth

--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -1,6 +1,6 @@
 const RegisterLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-gray-900 text-gray-200 text-base">
+    <div className="h-screen w-screen flex justify-center bg-gray-900 text-gray-200 text-base">
       {children}
     </div>
   )
