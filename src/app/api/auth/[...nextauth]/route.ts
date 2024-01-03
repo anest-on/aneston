50fdf59c0544d0e0599f5d7a37b8226d512b2e7a
Matheus Adorno <matheusadorno11@gmail.com>
2024-01-03 13:31:42 -0300

    .

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,6 @@
-import { PrismaAdapter } from '@/lib/auth/prisma-adapter'
-import type { NextAuthOptions } from 'next-auth'
+import { authOptions } from '@/utils/authOptions'
 import NextAuth from 'next-auth/next'
-import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
-
-const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(),
-
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
-        },
-      },
-      profile(profile: GoogleProfile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          user_link: '',
-          city: '',
-          state: '',
-          avatar_url: profile.picture,
-        }
-      },
-    }),
-  ],
-
-  callbacks: {
-    async signIn({ account }) {
-      if (
-        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-      ) {
-        return '/register/?error=permissions'
-      }
-
-      return true
-    },
-    async session({ session, user }) {
-      return {
-        ...session,
-        user,
-      }
-    },
-  },
-}
 
 const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST, authOptions }
+export { handler as GET, handler as POST }
---
+++ src/utils/authOptions.ts
@@ -0,0 +1,49 @@
+import { PrismaAdapter } from '@/lib/auth/prisma-adapter'
+import type { NextAuthOptions } from 'next-auth'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(),
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      authorization: {
+        params: {
+          scope:
+            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+        },
+      },
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          user_link: '',
+          city: '',
+          state: '',
+          avatar_url: profile.picture,
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    async signIn({ account }) {
+      if (
+        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+      ) {
+        return '/register/?error=permissions'
+      }
+
+      return true
+    },
+    async session({ session, user }) {
+      return {
+        ...session,
+        user,
+      }
+    },
+  },
+}
