f924c3ff797671d00ff2c766b44de4014cbe7a7b
Matheus Adorno <matheusadorno11@gmail.com>
2024-03-10 19:18:40 -0300

    TimeIntervals Layout

---
+++ src/app/time-intervals/layout.tsx
@@ -0,0 +1,9 @@
+const TimeIntervalsLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col justify-center h-full w-screen bg-gray-900 text-gray-200 text-base">
+      {children}
+    </div>
+  )
+}
+
+export default TimeIntervalsLayout
