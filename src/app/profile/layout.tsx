c40865ac8a256565814c07a451746156292ec853
Matheus Adorno <matheusadorno11@gmail.com>
2024-03-10 19:15:07 -0300

    profile layout update

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -1,6 +1,6 @@
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center h-full w-screen bg-gray-900 text-gray-200 text-base">
+    <div className="flex flex-col justify-center h-full w-screen bg-gray-900 text-gray-200 text-base">
       {children}
     </div>
   )
