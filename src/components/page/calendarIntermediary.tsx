c831ee5c196b06b5091f21ef56d21dcb0d39192e
JoaoPedroCamargo <j_souza_camargo@live.com>
2024-03-10 18:19:49 -0300

    calendar fixed

--- src/components/page/calendarIntermediary.tsx
+++ src/components/page/calendarIntermediary.tsx
@@ -38,32 +38,32 @@ const CalendarIntermediary = ({
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <div isTimePickerOpen={isDateSelected}>
-          <Calendar
-            selectedDate={selectedDate}
-            onDateSelected={setSelectedDate}
-          />
+        {/* <div isTimePickerOpen={isDateSelected}> */}
+        <Calendar
+          selectedDate={selectedDate}
+          onDateSelected={setSelectedDate}
+        />
 
-          {isDateSelected && (
-            <div className="border-l-[1px] bg-gray-800 border-l-gray-600 pt-6 pl-6 pb-0 overflow-y-auto absolute top-0 bottom-0 right-0 w-[280px]">
-              <div className="flex items-center justify-between">
-                <p className="font-medium">
-                  {weekDay},{' '}
-                  <span className="text-gray-200">{describedDate}</span>
-                </p>
-                <button
-                  type="button"
-                  onClick={() => setSelectedDate(null)}
-                  className="border-0 bg-gray-800 cursor-pointer text-gray-100 rounded-sm text-sm items-center pt-1 mr-4"
-                >
-                  <X size="19" />
-                </button>
-              </div>
-
-              <div className="mt-3 grid grid-cols-1 gap-2"></div>
+        {isDateSelected && (
+          <div className="border-l-[1px] bg-gray-800 border-l-gray-600 pt-6 pl-6 pb-0 overflow-y-auto absolute top-0 bottom-0 right-0 w-[280px]">
+            <div className="flex items-center justify-between">
+              <p className="font-medium">
+                {weekDay},{' '}
+                <span className="text-gray-200">{describedDate}</span>
+              </p>
+              <button
+                type="button"
+                onClick={() => setSelectedDate(null)}
+                className="border-0 bg-gray-800 cursor-pointer text-gray-100 rounded-sm text-sm items-center pt-1 mr-4"
+              >
+                <X size="19" />
+              </button>
             </div>
-          )}
-        </div>
+
+            <div className="mt-3 grid grid-cols-1 gap-2"></div>
+          </div>
+        )}
+        {/* </div> */}
       </QueryClientProvider>
     </>
   )
