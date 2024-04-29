b849f6c4b29de9dc991852112737484c965f4d4e
teller <165670905+TellerSaude@users.noreply.github.com>
2024-04-29 15:12:39 -0300

    fix: removed testeCalendar file

--- src/app/login/[user_link]/testCalendar/layout.tsx
+++
@@ -1,9 +0,0 @@
-const RegisterLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex justify-center h-full w-screen bg-gray-900 text-gray-200 text-base">
-      {children}
-    </div>
-  )
-}
-
-export default RegisterLayout
--- src/app/login/[user_link]/testCalendar/page.tsx
+++
@@ -1,7 +0,0 @@
-import CalendarPage from '@/components/page/calendarPage'
-
-const Register = async ({ params }: { params: { user_link: string } }) => {
-  return <CalendarPage params={params} />
-}
-
-export default Register
