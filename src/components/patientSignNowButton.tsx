049ae494f356448f406a3b92c60677ff5a57c7b0
Matheus Adorno <matheusadorno11@gmail.com>
2024-07-08 15:22:01 -0300

    feat: Button on dashboard that let patient sign form in person with the doctor

---
+++ src/components/patientSignNowButton.tsx
@@ -0,0 +1,30 @@
+import { FileSignature } from 'lucide-react'
+import { Button } from './ui/button'
+import { useRouter } from 'next/navigation'
+
+interface PatientSignNowButtonProps {
+  formId: string
+  children?: React.ReactNode
+}
+
+export default function PatientSignNowButton({
+  formId,
+  children,
+}: PatientSignNowButtonProps) {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push(`/consultation-certificate/${formId}`)
+  }
+
+  return (
+    <Button
+      variant={'ghost'}
+      className="hover:text-gray-40 w-full h-full gap-1 flex fles-row justify-start p-2"
+      onClick={handleClick}
+    >
+      <FileSignature className="w-4 h-4 hover:text-gray-400 hover:cursor-pointer" />
+      {children}
+    </Button>
+  )
+}
